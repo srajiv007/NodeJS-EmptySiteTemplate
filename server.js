79e0488f2878d413db45d2e8b0718a8cd40828ea
Rajiv S <s.rajivs@gmail.com>
2018-04-06 18:07:31 -0700

    new changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = require('./app');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('test1');
+    res.write('hello');
     res.end();
     
 }).listen(process.env.PORT || 8080);
