f9da1043186b442674e1ac7f5ffcdbdda93991fe
Rajiv S <s.rajivs@gmail.com>
2018-04-06 18:05:47 -0700

    test changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = require('./app');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('test');
+    res.write('test1');
     res.end();
     
 }).listen(process.env.PORT || 8080);
